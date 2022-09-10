ed6ca78994567ea03c86df0f5864ced0057b3627
JMwill <willsurpriseyou@foxmail.com>
2022-09-10 11:33:31 +0800

    update Auto-Commit.bat

--- Auto-Commit.bat
+++ Auto-Commit.bat
@@ -4,7 +4,7 @@ echo "DOCS PUSH BAT"
 ::移动到需要自动提交的文件夹内
 echo "1. Move to working directory"
 C:
-cd C:\Users\Administrator\Documents\logseq
+cd C:\Users\%USERNAME%\Documents\logseq
 
 echo "2. Start GitAutoCommit.ps1"
 ::执行powershell脚本，设置远程分支和本地分支，设置每隔一段时间执行一次，自动 push
